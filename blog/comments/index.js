7164c8a93b699ffcc404a044314ee2da8837e9ee
fenilhansaliami <fenil.hansalia@mindinventory.com>
2021-02-05 16:08:07 +0530

    query service added

--- blog/comments/index.js
+++ blog/comments/index.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const { randomBytes } = require("crypto");
 const cors = require("cors");
 const app = express();
+const axios = require("axios");
 
 const commentsByPostId = {};
 app.use(cors());
@@ -12,17 +13,54 @@ app.get("/posts/:id/comments", (req, res) => {
   res.status(200).send(commentsByPostId[req.params.id] || []);
 });
 
-app.post("/posts/:id/comments", (req, res) => {
+app.post("/posts/:id/comments", async (req, res) => {
   const id = randomBytes(4).toString("hex");
   const { content } = req.body;
 
   const comments = commentsByPostId[req.params.id] || [];
-  comments.push({ id: id, content });
+  comments.push({ id: id, content, status: "pending" });
   commentsByPostId[req.params.id] = comments;
+
+  await axios.post("http://localhost:4005/events", {
+    type: "CommentCreated",
+    data: {
+      id: id,
+      content: content,
+      postId: req.params.id,
+      status: "pending",
+    },
+  });
 
   res.status(201).send(comments);
 });
 
+app.post("/events", async (req, res) => {
+  console.log("Received Event", req.body.type);
+  const { type, data } = req.body;
+
+  if (type === "CommentModerated") {
+    const { postId, id, status, content } = data;
+    const comments = commentsByPostId[postId];
+
+    const comment = comments.find((comment) => {
+      return comment.id === id;
+    });
+    comment.status = status;
+
+    await axios.post("http://localhost:4005/events", {
+      type: "CommentUpdated",
+      data: {
+        id,
+        status,
+        postId,
+        content,
+      },
+    });
+  }
+
+  res.send({});
+});
+
 app.listen(4001, () => {
-  console.log("Server is running on 4001");
+  console.log("Comment server is running on 4001");
 });
